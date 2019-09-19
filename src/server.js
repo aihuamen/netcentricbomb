fbf38d2aeb074012a758fbc0b96d3c06a30309c6
Sarun <sarunguy@hotmail.com>
2019-09-19 09:47:14 +0700

    Game mech finish

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const io = require("socket.io")();
 const port = 8000;
+const { createBoard, calculateScore } = require("../src/utils/game");
 
 io.listen(port);
 console.log("listening on port ", port);
 
-io.on("connection", client => {
+io.on("connection", socket => {
   console.log("Connected");
 
-  client.on("subscribeToTimer", interval => {
+  const board = createBoard();
+
+  socket.on("subscribeToTimer", interval => {
     console.log("client is subscribing to timer with interval ", interval);
     setInterval(() => {
-      client.emit("timer", "Yeet from server: " + new Date());
+      socket.emit("timer", "from server: " + new Date());
     }, interval);
+  });
+
+  socket.on("resetBoard", () => {
+    board = createBoard();
   });
 });
